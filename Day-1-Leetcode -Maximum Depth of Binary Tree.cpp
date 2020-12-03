444c5ae4a906a1ce0df7d4b8350483495243e336
Pawan Giri <49859885+123pk@users.noreply.github.com>
2020-12-03 19:16:48 +0530

    Update Day-1-Leetcode -Maximum Depth of Binary Tree.cpp

--- Day-1-Leetcode -Maximum Depth of Binary Tree.cpp
+++ Day-1-Leetcode -Maximum Depth of Binary Tree.cpp
@@ -1,3 +1,20 @@
+/*
+Platform :- Leetcode
+Problem :- Maximum Depth of Binary Tree
+Problem statement:- 
+Given the root of a binary tree, return its maximum depth.
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+ 
+Example 2:
+Input: root = []
+Output: 0
+ 
+Approach :- idea is to do level order traversal and max levl is height of tree
+*/
+
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
@@ -20,23 +37,4 @@ public:
     }
 };
 
-<!---
-Given the root of a binary tree, return its maximum depth.
 
-A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
-Example 1:
-Input: root = [3,9,20,null,null,15,7]
-Output: 3
- 
-Example 2:
-Input: root = [1,null,2]
-Output: 2
- 
-Example 3:
-Input: root = []
-Output: 0
- 
-Example 4:
-Input: root = [0]
-Output: 1
---->
