adff94fa942f72eb068ab975d48e535d42530da8
Pawan Giri <49859885+123pk@users.noreply.github.com>
2020-12-06 11:14:33 +0530

    Update Goal Parser Interpretation.cpp

--- Day6/Leetcode/Goal Parser Interpretation.cpp
+++ Day6/Leetcode/Goal Parser Interpretation.cpp
@@ -1,1 +1,40 @@
-
+/*
+Platform :- Leetcode
+Question :- Global Parser Interpretation ( Weekly contest 218 )
+Approach :- check for (),(al) & G - in the string and replace with desired characters 
+*/
+class Solution {
+public:
+    string interpret(string c) {
+        string ans;
+        int f=0;
+        for(int i=0;i<c.size();++i){
+            if(c[i]=='G'){
+                ans+=c[i];
+            }
+            else{
+                if(f){
+                    if(c[i]==')')
+                    {
+                        ans+='o';
+                        f=0;
+                    }
+                    else
+                    {
+                        ans+="al";
+                        i+=2;
+                        f=0;
+                    }
+                }
+                else 
+                {
+                    if(c[i]=='('){
+                        f=1;
+                    }
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
