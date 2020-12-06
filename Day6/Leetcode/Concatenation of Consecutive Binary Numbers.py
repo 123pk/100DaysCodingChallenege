48fcde8eb7ea25f17d53da4db7ef3e032e81985e
Pawan Giri <49859885+123pk@users.noreply.github.com>
2020-12-06 11:27:55 +0530

    Update Concatenation of Consecutive Binary Numbers.py

--- Day6/Leetcode/Concatenation of Consecutive Binary Numbers.py
+++ Day6/Leetcode/Concatenation of Consecutive Binary Numbers.py
@@ -1,11 +1,11 @@
-/*
+"""
 Platform :- Leetcode
 Question :- Concatenation of Consecutive Binary Numbers
 Approach :- run a loop from (i = 1 to  i = n) and change 'i' to binary form which we can do easily using 'bin()'
            i.'bin()' - it convert number into binary form with unwanted prefix '0b' which we can remove by string slicing and add to our answer string
            ii. At the end we will convert binary string into integer . there are various ways one way which I used is 'int(string,2)'
            iii. return answer by doing mod of the integer value
-*/
+"""
 class Solution:
     
     def concatenatedBinary(self, n: int) -> int:
