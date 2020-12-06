5868922f7aed99f8bc03b11f2e144b16ce78674f
Pawan Giri <49859885+123pk@users.noreply.github.com>
2020-12-06 22:25:56 +0530

    Create Avoid Trygub.cpp

---
+++ Day6/Codeforces/Avoid Trygub.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Codeforces
+Question :- Avoid Trygub ( Codeforces Global round 12)
+Approach :- It will be added after contest is over
+           Code will be added at 11:05 pm as soon as contest ends
+*/
