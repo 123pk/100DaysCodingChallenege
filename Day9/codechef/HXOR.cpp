55ff7b73003b1f3f844c87ef8aa33d832efe3979
Pawan Giri <49859885+123pk@users.noreply.github.com>
2020-12-09 17:56:35 +0530

    Create HXOR.cpp

---
+++ Day9/codechef/HXOR.cpp
@@ -0,0 +1,30 @@
+/*
+COMPLETE SOLUTION WILL BE ADDED AFTER CONTEST IS OVER 
+
+Platform :- Codechef
+Question :- Hail Xor ( Codechef long 2020 December )
+Approach :- It would be add after contest is over 
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+   
+   int t;
+   cin>>t;
+   
+   while(t--){
+       int n;
+       long int x;
+       cin>>n>>x;
+       
+       long int A[n];
+       for(int i=0;i<n;++i){
+          cin>>A[i];
+       }
+       
+       // COMPLETE SOLUTION WILL BE ADDED AFTER CONTEST IS OVER 
+   }
+}
