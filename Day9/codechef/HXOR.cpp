722378c9f7e26a39f98b62231d5136f1d2836daa
Pawan Giri <49859885+123pk@users.noreply.github.com>
2020-12-14 19:27:49 +0530

    Update HXOR.cpp

--- Day9/codechef/HXOR.cpp
+++ Day9/codechef/HXOR.cpp
@@ -3,28 +3,69 @@ COMPLETE SOLUTION WILL BE ADDED AFTER CONTEST IS OVER
 
 Platform :- Codechef
 Question :- Hail Xor ( Codechef long 2020 December )
-Approach :- It would be add after contest is over 
+Approach :- For each A[i] we will try to find A[j] such that i<j<=n-1 and (A[i] xor p < A[i]) && (A[j] xor p < A[j]) greedily 
 
 */
 
 #include<bits/stdc++.h>
 using namespace std;
 
+
 int main(){
    
-   int t;
-   cin>>t;
-   
-   while(t--){
-       int n;
-       long int x;
-       cin>>n>>x;
-       
-       long int A[n];
-       for(int i=0;i<n;++i){
-          cin>>A[i];
-       }
-       
-       // COMPLETE SOLUTION WILL BE ADDED AFTER CONTEST IS OVER 
-   }
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        long int x;
+        cin>>n>>x;
+        
+        long int A[n];
+        int c=0;
+        for(int i=0;i<n;++i){
+            cin>>A[i];
+            if(A[i]%2)c++;
+        }
+      
+        int j=0;
+        int i=0;
+        while(j<(n-1)){
+            if(A[j]==0){
+                j++;
+                continue;
+                
+            }
+            long int temp = A[j];
+            long int xx=1,z;
+            while(xx<temp){
+                xx*=2;
+            }
+            if(xx!=temp){
+                xx/=2;
+            }
+            z=xx;
+            int q;
+            for(q=j+1;q<n-1;q++){
+                if((A[q] xor z)<A[q]){
+                    break;
+                }
+                
+            }
+            A[j]=A[j] xor z;
+            A[q]=A[q] xor z;
+            i++;
+            if(i==x)break;
+        }
+         
+         long int final=x-i;
+        if((n==2 && final>0) && (final%2!=0)){
+            A[n-1]= A[n-1] xor 1;
+            A[n-2]= A[n-2] xor 1;
+        }
+        
+        for(auto x:A)cout<<x<<" ";
+        cout<<endl;
+    }
+    return 0;
 }
