2ccf2296b4912a53d96cd9cb38934edd6dd4a936
Pawan Giri <49859885+123pk@users.noreply.github.com>
2020-12-20 00:10:10 +0530

    Create Hidden Number.cpp

---
+++ Day19/Hidden Number.cpp
@@ -0,0 +1,23 @@
+/*
+Platform :- Codechef
+Problem :- Hidden Number (UWCOI21A)
+Approach :- print 1 and n as 1*n=n
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+   
+int t;
+cin>>t;
+while(t--){
+    long long n;
+    cin>>n;
+    
+    cout<<1<<" "<<n<<endl;
+   
+}
+   
+}
