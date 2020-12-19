b10bab098d699d75311661c2dbefdda812b69dfd
Pawan Giri <49859885+123pk@users.noreply.github.com>
2020-12-19 20:44:58 +0530

    Create A-Brick.cpp

---
+++ Day19/A-Brick.cpp
@@ -0,0 +1,16 @@
+/*
+Platform :- Atcoder
+Problem :- A- Brick
+Approach :- cost of 1 pen is k , you have n rupees number of pens you can buy ? this is same problem print (n/k)
+
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long n,k;
+    cin>>n>>k;
+    
+    long long ans=n/k;
+    cout<<ans<<endl;
+}
