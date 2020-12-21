46b1071180a3a4ff4f46437bff72f7dff331052c
Pawan Giri <49859885+123pk@users.noreply.github.com>
2020-12-21 06:28:59 +0530

    Update A-In Game Chat.cpp

--- Day20/A-In Game Chat.cpp
+++ Day20/A-In Game Chat.cpp
@@ -1,1 +1,43 @@
-Solution will be added once contest is over
+/*
+Platform :- Codeforces
+Problem :- A- In Game Chat
+Approach :- let (c = count the the number of ')' ) from end and compare with ( n-c)
+
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        
+        string s;
+        cin>>s;
+        
+        int f=0;
+        int d=0;
+       
+        for(int i=n-1;i>=0;--i){
+            if('a'<=s[i] && s[i]<='z'){
+                break;
+            }
+            else{
+                d++;
+            }
+        }
+        
+        int c=n-d;
+        
+        if(c>=d){
+            cout<<"No"<<endl;
+        }
+        else{
+            cout<<"Yes"<<endl;
+        }
+    }
+}
