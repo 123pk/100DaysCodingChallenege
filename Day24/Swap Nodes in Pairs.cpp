50a3da1c615cb51c9b87bd6df34b6dfd652940d0
Pawan Giri <49859885+123pk@users.noreply.github.com>
2020-12-24 21:42:59 +0530

    Update and rename Swap Nodes in Piars.cpp to Swap Nodes in Pairs.cpp

--- Day24/Swap Nodes in Pairs.cpp
+++ Day24/Swap Nodes in Pairs.cpp
@@ -1,19 +1,21 @@
+/*
+Platform :- Leecode
+Problem :- Swapping Nodes in Pairs
+*/
+
+
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
         if(!head)return NULL;
         if(!head->next)return head;
         ListNode*temp=head;
-        head=temp->next;
         ListNode*end=NULL;
         while(temp){
             if(temp->next){
-                ListNode*z=temp;
-                ListNode* z2=temp->next;
-                temp->next=z2->next;
-                z2->next=z;
-                if(z->next){
-                    end=z->next;
+                swap(temp->val,temp->next->val);
+                if(temp->next->next){
+                    end=temp->next->next;
                 
                 }else{
                     break;
