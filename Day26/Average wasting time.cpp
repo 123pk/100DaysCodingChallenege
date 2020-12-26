3bea0f89cb3224f76ce43ad899790c591f333e34
Pawan Giri <49859885+123pk@users.noreply.github.com>
2020-12-26 21:45:11 +0530

    Update Average wasting time.cpp

--- Day26/Average wasting time.cpp
+++ Day26/Average wasting time.cpp
@@ -1,3 +1,11 @@
+/*
+Platform :- Leetcode
+Problem :- Average Wasting time
+Approach :- we can clearly see that atleast all of them need to wait until their dish is cooked so min ( tot = (sum of all time for coocking their dishes))
+            we will take an temperory variable and it will tell us that if cook is free at time of order or not . if not we will add the waiting time to total 
+            value which is ( order completing time - order time )
+*/
+
 class Solution {
 public:
     double averageWaitingTime(vector<vector<int>>& c) {
