0fea1e86f6a507b668ba72a5f66579ffcabaad6f
Pawan Giri <49859885+123pk@users.noreply.github.com>
2020-12-29 22:37:51 +0530

    Create The Begining era of Cyberverse.cpp

---
+++ Day29/The Begining era of Cyberverse.cpp
@@ -0,0 +1,18 @@
+/*
+Platform :- Codehcef
+Problem :- The Begining era of Cyberverse
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long n,k;
+        cin>>n>>k;
+        
+        long long ans=k/n;
+        cout<<ans<<endl;
+    }
+}
