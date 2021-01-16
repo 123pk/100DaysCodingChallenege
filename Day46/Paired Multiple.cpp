e6a3e43a6248fdada12d37d9f53e1c4920fa1357
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-01-16 22:06:28 +0530

    Update Paired Multiple.cpp

--- Day46/Paired Multiple.cpp
+++ Day46/Paired Multiple.cpp
@@ -10,43 +10,20 @@ using namespace std;
 class  PairedMultiples{
     public:
     int selectedTotal(vector <int> A, int c){
-         map<int,int>P;
-         vector<pair<int,int>>Q;
-         vector<int>R;
-         //int e=0;
-         for(int i=0;i<A.size();++i){
-             if((A[i]%c)!=0){
-                 Q.push_back({A[i],A[i]%c});
-    
-                P[A[i]%c]++; 
-             }
-             else{
-                 //cout<<A[i]<<" $ \n";
-                 Q.push_back({A[i],0});
-                 R.push_back(A[i]);
-             }
-             
-         }
          int d=0;
-         
-         for(int i=0;i<Q.size();++i){
-             if(Q[i].second){
-             int temp=c-Q[i].second;
-             if(temp){
-                 if(P[temp] && P[Q[i].second]){
-                     d+=Q[i].first;
-                     
+         for(int i=0;i<A.size();++i){
+             
+             for(int j=0;j<A.size();++j){
+               if(i==j)continue;
+                 if((A[i]+A[j])%c==0)
+                 {
+                     d+=A[i];
+                     break;
                  }
              }
-             }
          }
-         
-         if(R.size()>1){
-            //cout<<" $ ";
-         for(auto x:R){
-             d+=x;
-         }
-         }
+          
+          
          return d;
      }
 };
