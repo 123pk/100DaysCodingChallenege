e582d96d9ca9d89878d5c0a0c1aaf9b86964c029
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-01-23 21:41:28 +0530

    Create A - Slot.cpp

---
+++ Day54/A - Slot.cpp
@@ -0,0 +1,18 @@
+/*
+Platform :- Atcoder
+Problem :- A - Slot
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    char ch,ch1,ch2;
+    cin>>ch>>ch1>>ch2;
+    if((ch==ch1)&&(ch==ch2)){
+        cout<<"Won"<<endl;
+    }
+    else{
+        cout<<"Lost"<<endl;
+    }
+}
