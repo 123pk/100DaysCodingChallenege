bdbc52bf54369c1014cc62cf36dd03d44ebf6f54
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-02-06 21:38:12 +0530

    Create Maximum length of String After deleting Similar ends.cpp

---
+++ Day68/Maximum length of String After deleting Similar ends.cpp
@@ -0,0 +1,45 @@
+/*
+Platform :- Leetcode
+Contest :- Biweekly contest 45
+Problem :- Maximum length of String After deleting Similar ends
+*/
+
+class Solution {
+public:
+    int minimumLength(string s) {
+        int start=0,end=s.size()-1;
+        int tot=s.size();
+        while(start< end){
+            if(s[start]!=s[end]){
+                break;
+            }
+            int c=1;
+            int i=start+1;
+            while(i<end){
+                if(s[start]==s[i]){
+                    i++;
+                    c++;
+                }
+                else break;
+            }
+            
+            int j=end-1;
+            int d=1;
+            if(i!=end){
+            while(start<j){
+                if(s[end]==s[j]){
+                    j--;
+                    d++;
+                }
+                else break;
+            }
+            }
+              
+            tot-=(c+d);
+            start=i;
+            end=j;
+             
+        }
+        return tot;
+    }
+};
