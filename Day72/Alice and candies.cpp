b765143ed31988bb24eb0ef0ac68987a3d8ea0c3
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-02-11 00:39:29 +0530

    Update Alice and candies.cpp

--- Day72/Alice and candies.cpp
+++ Day72/Alice and candies.cpp
@@ -1,107 +1,29 @@
+/*
+Platform :- Hackerearth
+Problem :- Alice and Candies
+Contest :- February easy
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
-
-int main(){
-	long long n;
-	cin>>n;
-   
-     int z=0;
-	 long long temp=0,d=1;
-	 while(temp<n){
-		 temp+=d;
-		 d+=2;
-		 z++;
-	 }
-	// cout<<z<<endl;
-	 if(n%2==0){
-           int d=2;
-		   int ans=0;
-		   while(d<=z){
-			   if(n%d==0){
-                  int c=d/2;
-				  int k=c;
-				  long int temp1=n/d,temp2=n/d;
-				  if(temp1%2==0){
-				      temp1--;
-				  }
-				  if(temp2%2==0){
-				  temp2++;
-				  }
-				  else{
-				      temp2+=2;
-				  }
-				  long int temp=0;
-				 //  cout<<temp1<<" "<<temp2<<endl;
-				  for(long int i=temp1;i>1 && c>0;i-=2){
-                      temp+=i;
-					  c--;
-				  }
-
-				  for(long int i=temp2;k;i+=2){
-					  temp+=i;
-					  k--;
-				  }
-				  //cout<<temp<<" "<<ans<<endl;
-
-				  if(temp==n)ans++;
-				  else {
-					  if(temp>n)break;
-				  }
-			   }
-			   else{
-				   if(z<d)break;
-			   }
-			   d+=2;
-		   }
-		   cout<<ans<<endl;
-	 }
-	 else{
-	     
-		 int f=0;
-		 int ans=0;
-		 int d=1;
-		  
-		 while(d<=z){
-			  
-			 if(d==1){
-				 ans++;
-			 }
-			 else{
-			     //cout<<n<<" "<<d<<endl;
-                if(n%d==0){
-					int c=(d-1)/2;
-					int k=c;
-                  long int temp=n/d;
-                  long int zz=temp;
-                  // cout<<k<<" "<<c<<" "<<temp<<endl;
-				  for(int i=zz-2;c>0 && i>1;i-=2){
-					  temp+=i;
-					  c--;
-				  }
-				   
-				  for(int i=zz+2;k>0;i+=2){
-					  temp+=i;
-					  k--;
-				  }
-				   //  cout<<temp<<" "<<d<<endl;
-
-				  if(temp>n){
-					  break;
-				  }
-				  else {
-					  if(temp==n){
-						  ans++;
-					  }
-				  }
-				}
-				else{
-					if(z<d)break;
-				}
-				 
-			 }
-			 d+=2;
-
-		 }
-		 cout<<ans<<endl;
-	 }
+ 
+int main()
+{
+    long long n;
+    cin>>n;
+	
+    int ans=0;
+    for(long long i=1;i*i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            int x=i;
+            int y=n/i;
+            if((x+y)%2==0)
+            {
+                cnt++;
+            }
+        }
+    }
+    cout<<ans<<endl;
 }
