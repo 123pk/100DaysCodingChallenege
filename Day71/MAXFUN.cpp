5765a0b30e6a0aebb48e77cc0a5e6f7547a7e5da
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-02-18 14:34:26 +0530

    Update MAXFUN.cpp

--- Day71/MAXFUN.cpp
+++ Day71/MAXFUN.cpp
@@ -6,3 +6,29 @@ Problem :- Maximise Sum ( MAXSUM)
 
 SOLUTION WILL BE ADDED ONCE CONTEST IS OVER
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n;
+        cin>>n;
+        
+        long int A[n];
+        for(int i=0;i<n;++i){
+            cin>>A[i];
+        }
+        
+        sort(A,A+n);
+        
+        long long temp1=(A[1]-A[0]) + (A[n-1]-A[0]) + (A[n-1]-A[1]);
+        long long temp2=(A[n-1]-A[n-2]) + (A[n-1]-A[0]) + (A[n-2]-A[0]);
+        
+        long long ans=max(temp1,temp2);
+        cout<<ans<<endl;
+    }
+}
