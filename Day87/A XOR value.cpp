e2525e5d12c4273e2c4e3b200d282e15fa6eb72c
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-02-27 23:07:56 +0530

    Update A XOR value.cpp

--- Day87/A XOR value.cpp
+++ Day87/A XOR value.cpp
@@ -6,3 +6,36 @@ Problem :- A XOR value
 
 SOLUTION WILL BE ADDED ONCE CONTEST IS OVER
 */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+
+		long long A[n];
+		map<int,int>P;
+		long long ans=0;
+		for(int i=0;i<n;++i){
+			cin>>A[i];
+			string s=bitset<64>(A[i]).to_string();
+			reverse(s.begin(),s.end());
+            for(int j=0;j<s.size();++j){
+				if(s[j]=='1'){
+					P[j+1]++;
+				}
+			}
+		}
+        
+		for(auto x:P){
+			if(x.second>(n/2)){
+				ans+=pow(2,x.first-1);
+			}
+		}
+		 
+		cout<<ans<<endl;
+	}
+}
