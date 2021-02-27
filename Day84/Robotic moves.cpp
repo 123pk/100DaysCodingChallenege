cd1dfc3318ae5811324c5630a0224e6f9ed95ff7
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-02-27 23:10:39 +0530

    Update Robotic moves.cpp

--- Day84/Robotic moves.cpp
+++ Day84/Robotic moves.cpp
@@ -6,3 +6,18 @@ Problem :- Robotic moves
 
 SOLUTION WILL BE ADDED ONCE CONTEST IS OVER
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		long long n;
+		cin>>n;
+
+		n*=(n+1);
+		cout<<n<<endl;
+	}
+}
