c39d8b75a3f8bddcb5ebe38d86951ca1eca745fd
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-02-27 23:09:48 +0530

    Update The smallest string.cpp

--- Day85/The smallest string.cpp
+++ Day85/The smallest string.cpp
@@ -5,3 +5,62 @@ Problem :- The smallest string
 
   SOLUTION WILL BE ADDED ONCE CONTEST IS OVER
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int n,k;
+		cin>>n>>k;
+
+		string s;
+		cin>>s;
+
+		for(int i=0;i<n;++i){
+			if(s[i]=='a'){
+                if(k){
+					if(i==n-1){
+						k%=26;
+						s[i]=s[i]+k;
+					}
+
+				}
+				else continue;
+			}
+			else{
+				if(k>0){
+					
+					int temp=26-(s[i]-'a');
+					if(k>=temp){
+						k-=temp;
+						s[i]='a';
+						if(i==n-1){
+							k%=26;
+							s[i]=s[i]+k;
+							k-=k;
+							break;
+						}
+					}
+					else{
+						if(i==n-1){
+							k%=26;
+							///cout<<s[i]<<" %% "<<k<<endl;
+							s[i]=s[i]+k;
+							k-=k;
+						}
+						else{
+							continue;
+						}
+					}
+ 
+				}
+				else break;
+			}
+			//cout<<s<<" "<<k<<endl;
+		}
+		cout<<s<<endl;
+	}
+}
