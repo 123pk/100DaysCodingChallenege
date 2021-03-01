816b1b44f6681f47b8c846cbe5d8da9afa798a4f
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-03-01 21:12:49 +0530

    Create Distribute Candies.cpp

---
+++ Day91/Distribute Candies.cpp
@@ -0,0 +1,20 @@
+/*
+Platform :- Leetcode
+Problem :- Distribute Candies
+Event :- March Challenge
+*/
+
+class Solution {
+public:
+    int distributeCandies(vector<int>& A) {
+        set<int>P;
+        for(int i=0;i<A.size();++i){
+            P.insert(A[i]);
+        }
+        
+        int k=P.size();
+        int n=A.size();
+        n/=2;
+        return min(k,n);
+    }
+};
