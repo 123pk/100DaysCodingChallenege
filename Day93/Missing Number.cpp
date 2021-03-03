66577aaf09c4351f1da68c0e9dbd06cfef350bf4
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-03-03 19:15:28 +0530

    Create Missing Number.cpp

---
+++ Day93/Missing Number.cpp
@@ -0,0 +1,17 @@
+/*
+Platform :- Leetcode
+Problem :- Missing Number
+*/
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<(nums.size());++i){
+            if(nums[i]!=i){
+                return i;
+            }
+        }
+        return nums.size();
+    }
+};
