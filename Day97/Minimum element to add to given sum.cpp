df962f5cceacde92783be28907f79c7790874e80
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-03-07 18:10:13 +0530

    Create Minimum element to add to given sum.cpp

---
+++ Day97/Minimum element to add to given sum.cpp
@@ -0,0 +1,20 @@
+/*
+Pltform :- Leetcode
+Problem :- Minimum elemnts to add to given sum
+Contest :- Weekly contest
+*/
+
+class Solution {
+public:
+    int minElements(vector<int>& nums, int limit, int goal) {
+        long long tot=0;
+        for(int i=0;i<nums.size();++i){
+            tot+=nums[i];
+        }
+        if(tot==goal)return 0;
+        long long dif=abs(tot-goal);
+        int temp=dif/limit;
+        if(dif%limit)temp++;
+        return temp;
+    }
+};
