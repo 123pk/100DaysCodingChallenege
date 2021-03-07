b34d2fcd2cd6d758114be1361e29ad61e480d651
Pawan Giri <49859885+123pk@users.noreply.github.com>
2021-03-07 18:08:09 +0530

    Create check if binary string has atmost one segment of ones.cpp

---
+++ Day97/check if binary string has atmost one segment of ones.cpp
@@ -0,0 +1,28 @@
+/*
+Platform :- Leetcode
+Problem :- Check if binary string has atmost one segment of ones
+Contest:- Weekly contest 
+*/
+
+class Solution {
+public:
+    bool checkOnesSegment(string s) {
+        int f=0;
+        int c=0;
+        for(int i=0;i<s.size();++i){
+            if(f){
+                if(s[i]=='0'){
+                    f=0;
+                }
+                
+            }
+            else{
+                if(s[i]=='1'){
+                    f=1;c++;
+                }
+            }
+        }
+        if(c==1)return true;
+        return false;
+    }
+};
